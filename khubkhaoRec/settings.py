e1558e4fb2212e371cd3f124a01a0316253c9b3a
NokKbl <asdfghjklnokky@gmail.com>
2018-10-23 23:59:37 +0700

    change to postgresql

--- khubkhaoRec/settings.py
+++ khubkhaoRec/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'khubkhaoRec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'khubkhao_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pxz',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
