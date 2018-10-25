2e502b97814cf611b6b49d014ec65acaffde75c6
NokKbl <asdfghjklnokky@gmail.com>
2018-10-25 12:29:03 +0700

    edit requirement

--- khubkhaoRec/settings.py
+++ khubkhaoRec/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['khubkhao-rec.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'khubkhaoapp',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
