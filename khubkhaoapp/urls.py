7f2d9a116bea5e46802a2e5bccc48cf02b629815
NokKbl <asdfghjklnokky@gmail.com>
2018-10-30 18:58:44 +0700

    change template_name to name

--- khubkhaoapp/urls.py
+++ khubkhaoapp/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), template_name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
