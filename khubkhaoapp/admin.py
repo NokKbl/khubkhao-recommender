bfaf90193ac39f0b826364e037907bc09c280d48
Kornphon Noiprasert <konphon22@hotmail.com>
2018-11-01 22:09:32 +0700

    Fix admin.py (wrong attribute name)

--- khubkhaoapp/admin.py
+++ khubkhaoapp/admin.py
@@ -5,7 +5,7 @@ class FoodAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,          {'fields': ['food_name']}),
         ('Details',     {'fields': ['image_location','average_price','rate']}),
-        ('Country',      {'fields': ['country']}),
+        ('Category',      {'fields': ['category']}),
         ('Ethnic Food',      {'fields': ['ethnic_food_name']}),
     ]
 
