473175e1d4dafc5099a5f0b120f802876c9506e1
New-az <viyotboon_new@outlook.com>
2018-11-03 06:23:44 +0700

    add_test_models

---
+++ khubkhaoapp/tests/tests_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from khubkhaoapp.models import Category, EthnicFood, Food
+
+
+class CategoryModelTest(TestCase):
+
+    def test_string_representation(self):
+        category = Category(type_name='Rice')
+        self.assertEqual(str(category),category.type_name)
+
+class EthnicFoodModelTest(TestCase):
+
+    def test_string_representation(self):
+        ethnic_food = EthnicFood(ethnic_food_name='ThaiFood')
+        self.assertEqual(str(ethnic_food),ethnic_food.ethnic_food_name)
+
+class FoodModelTest(TestCase):
+
+    def test_string_of_food_name_representation(self):
+        name = Food(food_name='Jok')
+        self.assertEqual(str(name),name.food_name)
+
+    def test_string_of_image_location_representation(self):
+        text = Food(image_location='/home/test')
+        self.assertEqual(Food.get_image_location(text),text.image_location)
+
+    def test_intenger_of_average_price_representation(self):
+        baht = Food(average_price=10)
+        self.assertEqual(Food.get_average_price(baht),baht.average_price)
+
+    def test_intenger_of_rate_representation(self):
+        number = Food(rate=10)
+        self.assertEqual(Food.get_rate(number),number.rate)
+
+    def test_objects_of_ethnic_food_name_representation(self):
+        ethnic_food = EthnicFood(ethnic_food_name='JapanFood')
+        text = Food(ethnic_food_name=ethnic_food)
+        self.assertEqual(Food.get_ethnic_food_name(text),text.ethnic_food_name)
