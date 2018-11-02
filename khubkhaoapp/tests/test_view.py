f7759e832ce87ab267b719a7064b4d8bb6860d22
NokKbl <asdfghjklnokky@gmail.com>
2018-11-02 19:44:15 +0700

    add one test and change components in index

--- khubkhaoapp/tests/test_view.py
+++ khubkhaoapp/tests/test_view.py
@@ -14,6 +14,12 @@ class IndexPageTest(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'khubkhaoapp/index.html')
 
-    def test_home_page_contains_correct_html(self):
+    def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        self.assertContains(response, '')
+        self.assertContains(response, '<title>KhubKhao Recommender</title>')
+
+    def test_home_page_does_not_contains_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'This should not be on the page.')
+
+    
