fec60c972a3ec535212fafb713d9ca3a759fa9fa
NokKbl <asdfghjklnokky@gmail.com>
2018-11-04 11:42:50 +0700

    make change on url after click submit button

--- khubkhaoRec/urls.py
+++ khubkhaoRec/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from khubkhaoapp.views import IndexResultView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('khubkhaoapp.urls')),
-    path('result/', IndexResultView ),#Delete later because it must be called from index.html not by type in browser
 ]
