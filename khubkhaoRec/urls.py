210799dba97a3e038728d3bd3b57ed6f597a9d0e
NokKbl <asdfghjklnokky@gmail.com>
2018-11-04 10:49:14 +0700

    Merge Pull Request dev-webpage

--- khubkhaoRec/urls.py
+++ khubkhaoRec/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from khubkhaoapp.views import IndexView, IndexResultView
+from django.urls import path, include
+from khubkhaoapp.views import IndexResultView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
+    path('', include('khubkhaoapp.urls')),
     path('result/', IndexResultView ),#Delete later because it must be called from index.html not by type in browser
 ]
