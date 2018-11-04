942ca348325849a2279cd4a1cd8236e8f57861c9
NokKbl <asdfghjklnokky@gmail.com>
2018-11-04 10:43:08 +0700

    fix and merge work

--- khubkhaoRec/urls.py
+++ khubkhaoRec/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from khubkhaoapp.views import IndexResultView
+from django.urls import path
+from khubkhaoapp.views import IndexView, IndexResultView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('khubkhaoapp.urls')),
+    path('', IndexView.as_view()),
     path('result/', IndexResultView ),#Delete later because it must be called from index.html not by type in browser
 ]
