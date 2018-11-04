248796f15ad10c44e43bf0f9b4ba8cd78fc335ac
NokKbl <asdfghjklnokky@gmail.com>
2018-11-04 17:44:02 +0700

    comment all test in test_view

--- khubkhaoapp/tests/test_view.py
+++ khubkhaoapp/tests/test_view.py
@@ -1,25 +1,25 @@
-from django.http import HttpRequest
-from django.test import TestCase
-from django.urls import reverse
+# from django.http import HttpRequest
+# from django.test import TestCase
+# from django.urls import reverse
 
 
-class IndexPageTest(TestCase):
+# class IndexPageTest(TestCase):
 
-    # def test_home_page_status_code(self):
-    #     response = self.client.get('/')
-    #     self.assertEquals(response.status_code, 200)
+#     def test_home_page_status_code(self):
+#         response = self.client.get('/')
+#         self.assertEquals(response.status_code, 200)
 
-    # def test_view_uses_correct_template(self):
-    #     response = self.client.get(reverse('khubkhaoapp:index'))
-    #     self.assertEquals(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'khubkhaoapp/index.html')
+#     def test_view_uses_correct_template(self):
+#         response = self.client.get(reverse('khubkhaoapp:index'))
+#         self.assertEquals(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'khubkhaoapp/index.html')
 
-    # def test_home_page_returns_correct_html(self):
-    #     response = self.client.get('/')
-    #     self.assertContains(response, '<h1>Generated Result!</h1>')
+#     def test_home_page_returns_correct_html(self):
+#         response = self.client.get('/')
+#         self.assertContains(response, '<h1>Generated Result!</h1>')
 
-    # def test_home_page_does_not_contains_incorrect_html(self):
-    #     response = self.client.get('/')
-    #     self.assertNotContains(response, 'This should not be on the page.')
+#     def test_home_page_does_not_contains_incorrect_html(self):
+#         response = self.client.get('/')
+#         self.assertNotContains(response, 'This should not be on the page.')
 
     
