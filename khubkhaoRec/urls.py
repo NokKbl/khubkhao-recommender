4f1bd15a79a22d7ec180df48c0d42e83317fbd78
NokKbl <asdfghjklnokky@gmail.com>
2018-11-04 10:19:26 +0700

    resolved conflict for pull request

--- khubkhaoRec/urls.py
+++ khubkhaoRec/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path
+from khubkhaoapp.views import IndexView, IndexResultView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('khubkhaoapp.urls')),
+    path('', IndexView.as_view()),
+    path('result/', IndexResultView ),#Delete later because it must be called from index.html not by type in browser
 ]
