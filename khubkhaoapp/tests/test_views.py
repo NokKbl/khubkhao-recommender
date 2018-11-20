f1fd1b3dcfc0e82cbd086f009faeef098261147e
New-az <viyotboon_new@outlook.com>
2018-11-20 14:40:23 +0700

    delete unused test

--- khubkhaoapp/tests/test_views.py
+++ khubkhaoapp/tests/test_views.py
@@ -14,14 +14,4 @@ class IndexPageTest(TestCase):
         response = self.client.get(reverse('khubkhaoapp:index'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'khubkhaoapp/index.html')
-
-    def test_home_page_returns_correct_html(self):
-        ''' Check request url contains component in index.html '''
-        response = self.client.get('/')
         self.assertContains(response, '<h1>Generated Result!</h1>')
-
-    def test_home_page_does_not_contains_incorrect_html(self):
-        '''Check request url does not contains component in index.html'''
-        response = self.client.get('/')
-        self.assertNotContains(response, 'This should not be on the page.')
-
