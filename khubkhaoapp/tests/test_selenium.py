67772715c1ebec96b7867bbfa4f1582144dde795
NokKbl <asdfghjklnokky@gmail.com>
2018-11-27 21:24:37 +0700

    Edit docstring and some function name.

--- khubkhaoapp/tests/test_selenium.py
+++ khubkhaoapp/tests/test_selenium.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from unittest import TestCase
 
+
 class UntitledTestCase(TestCase):
     
     def setUp(self):
-        '''setup chromedriver to run headless and login to website'''
+        '''
+        Setup Chrome driver to run headless and login to the application webpage.
+        '''
         options = Options()
         options.add_argument("--headless")
         self.driver = webdriver.Chrome(chrome_options=options)
@@ -19,10 +22,12 @@ class UntitledTestCase(TestCase):
         self.driver.find_element_by_id("password").clear()
         self.driver.find_element_by_id("password").send_keys("DevTester2018")
         self.driver.find_element_by_id("allow").click()
-    
+
+
     def test_only_ethnic_check(self):
-        ''' Result of food after select ethnic is in database 
-            this test is select ethnic:Chinese food.
+        '''
+        Test that the webpage show the results of selected ethnic food correctly.
+        This test will select Chinese food.
         '''
         food_all = list()
         driver = self.driver
@@ -36,9 +41,11 @@ class UntitledTestCase(TestCase):
         self.assertIn("Beef Chow Fun",food_all)
         driver.close()
 
+
     def test_only_category_check(self):
-        ''' Result of food after select category is in database 
-            this test is select category:Rice dish.
+        '''
+        Test that the webpage show the results of selected category correctly.
+        This test will select Rice dish.
         '''
         food_all = list()
         driver = self.driver
@@ -53,9 +60,11 @@ class UntitledTestCase(TestCase):
         
         driver.close()
 
-    def test_ethnic_and_category_check(self):
-        ''' Result of food after select ethnic and category is in database
-            this test is select ethnic:Chinese food and category:Noodles dish.
+
+    def test_ethnic_with_category_check(self):
+        '''
+        Test that the webpage show the results of selected ethnic food with category correctly.
+        This test will select Chinese food with Noodle dish.
         '''
         food_all = list()
         driver = self.driver
@@ -71,9 +80,11 @@ class UntitledTestCase(TestCase):
         
         driver.close()
 
-    def test_ethnic_and_multiple_category_check(self):
-        ''' Result of food after select ethnic and multiple category is in database
-            this test is select ethnic:Chinese food and category:Healthy food, Vegetarian food, Side dish.
+
+    def test_ethnic_with_multiple_categories_check(self):
+        '''
+        Test that the webpage show the results of selected ethnic food with categories correctly.
+        This test will select Chinese food with Healthy food, Vegetarian food and Side dish.
         '''
         food_all = list()
         driver = self.driver
--- khubkhaoapp/tests/tests_models.py
+++ khubkhaoapp/tests/tests_models.py
@@ -2,36 +2,60 @@ from django.test import TestCase
 from khubkhaoapp.models import Category, EthnicFood, Food
 
 
-
 class CategoryModelTest(TestCase):
+
     def setUp(self):
+        '''
+        Setup by create Category objects.
+        '''
         Category.objects.create(type_name="Rice dish")
         Category.objects.create(type_name="Healthy food")
-        
+    
+    
     def test_create_category(self):
-        '''Check Category that have been setup is create'''
+        '''
+        Test that the Category objects which have been setup is created.
+        '''
         self.assertEqual(2, Category.objects.count())
 
+
     def test_category_text(self):
-        '''Check Category have text same as objects that have been setup'''
+        '''
+        Test that the Category objects have correct detail.
+        '''
         self.assertTrue(Category.objects.filter(type_name="Rice dish"))
 
+
 class EthnicFoodModelTest(TestCase):
+
     def setUp(self):
+        '''
+        Setup by create EthnicFood objects.
+        '''
         EthnicFood.objects.create(ethnic_food_name="Thai Food")
         EthnicFood.objects.create(ethnic_food_name="Korean food")
 
+
     def test_create_ethnicFood(self):
-        '''Check EthnicFood that have been setup is create'''
+        '''
+        Test that the EthnicFood objects which have been setup is created.
+        '''
         self.assertEqual(2, EthnicFood.objects.count())
 
+
     def test_ethnicFood_text(self):
-        '''Check EthnicFood have text same as objects that have been setup'''
+        '''
+        Test that the EthnicFood objects have correct detail.
+        '''
         self.assertTrue(EthnicFood.objects.filter(ethnic_food_name="Thai Food"))
+
 
 class FoodModelTest(TestCase):
     
     def setUp(self):
+        '''
+        Setup by create Food objects.
+        '''
         Food.objects.create(food_name = "Baozi",
             image_location = "http://drive.google.com/uc?export=view&id=1zbH52fRGRqMGRMSnEi2BGTU4D--Gkoy5",
             average_price = 3.1,
@@ -50,39 +74,61 @@ class FoodModelTest(TestCase):
             ethnic_food_name = EthnicFood.objects.create(ethnic_food_name="Korean food"),
             )
 
-        
 
     def test_create_food(self):
-        '''Check EthnicFood that have been setup is create'''
+        '''
+        Test that the Food objects which have been setup is created.
+        '''
         self.assertEqual(2, Food.objects.count())
 
+
     def test_string_of_food_name_representation(self):
-        ''' Charfield of food_name in Food models have same string that create in setup '''
+        '''
+        Test that a Food object contains correct value of food_name.
+        '''
         self.assertTrue(Food.objects.filter(food_name="Baozi"))
 
+
     def test_string_of_image_location_representation(self):
-        ''' Charfield of image_location in Food models have same string that create in setup '''
+        '''
+        Test that a Food object contains correct value of image_location.
+        '''
         self.assertTrue(Food.objects.filter(image_location="http://drive.google.com/uc?export=view&id=1zbH52fRGRqMGRMSnEi2BGTU4D--Gkoy5"))
 
-    def test_integer_of_average_price_representation(self):
-        ''' DecimalField of average_price in Food models have same integer that create in setup '''
+
+    def test_value_of_average_price_representation(self):
+        '''
+        Test that a Food object contains correct value of average_price.
+        '''
         self.assertTrue(Food.objects.filter(average_price=3.1))
 
-    def test_integer_of_original_rate_representation(self):
-        ''' DecimalField of original_rate Food models have same integer that create in setup '''
+
+    def test_value_of_original_rate_representation(self):
+        '''
+        Test that a Food object contains correct value of original_rate.
+        '''
         self.assertTrue(Food.objects.filter(original_rate=80))
 
-    def test_integer_of_user_rate_representation(self):
-        ''' DecimalField of user_rate Food models have same integer that create in setup '''
+
+    def test_value_of_user_rate_representation(self):
+        '''
+        Test that a Food object contains correct value of user_rate.
+        '''
         self.assertTrue(Food.objects.filter(user_rate=0))
 
+
     def test_objects_of_ethnic_food_name_representation(self):
-        ''' ForeignKey of ethnic_food_name in Food models have same objects that create in setup '''
+        '''
+        Test that a Food object contains correct value of ForeignKey for ethnic_food_name.
+        '''
         ethnic_food = EthnicFood.objects.all().first()
         self.assertTrue(Food.objects.filter(ethnic_food_name=ethnic_food))
 
+
     def test_ManyToMany_of_category_representation(self):
-        ''' ManyToMany of category in Food models is relate on field of Category '''
+        '''
+        Test that ManyToMany of category in Food models show all categories which have been created.
+        '''
         category1 = Category(type_name='Rice')
         category1.save()
         category2 = Category(type_name='Healthy')
@@ -92,49 +138,77 @@ class FoodModelTest(TestCase):
         food.category.add(category2)
         self.assertEqual(list(food.category.all()), [category1, category2])
 
+
     def test_add_user_pk_method(self):
-        ''' Food name Baozi have 3 pk_vote which is 1,6,7 then add pk=3. So it equal to 1,6,7,3, '''
+        '''
+        Test that Food object contains correct primary key of users.
+        In this test, Food name Baozi have 3 pk_vote which are 1,6,7.
+        If add primary key with value of 3, it'll be equal to 1,6,7,3,.
+        '''
         food = Food.objects.all().first()
         food.add_user_pk('3')
         self.assertEqual(food.get_user_pk(),'1,6,7,3,')
-    
+
+
     def test_add_user_count_method(self):
-        ''' Food name Baozi have 3 people vote. User add one more vote to food. So it increase to 4'''
+        '''
+        Test that the amounnt of voted user is correct.
+        In this test, Food name Baozi have 3 people vote.
+        If user added one more vote to the food, it'll increase the value to be 4.
+        '''
         food = Food.objects.all().first()
         food.add_user_count()
         self.assertEqual(food.get_user_count(),4)
 
+
     def test_one_user_vote(self):
-        ''' Food name Baccalà Fritto have original rate 78, user rate is set to 80 so total rate is 78.4  '''
+        '''
+        Test that the computed rate is correct.
+        In this test, Food name Baccalà Fritto have original rate 78.
+        If user rate is 80, then the total rate of this food is 78.4.
+        '''
         food = Food.objects.all().last()
         food.set_user_rate(80)
         rate = food.compute_total_rate()
         self.assertEqual(rate,78.4)
 
-    def test_many_user_vote(self):
-        ''' Food name Baozi have 3 vote, origingal rate is 80, all user rate is 120. The total rate is 72  '''
+
+    def test_multiple_users_vote(self):
+        '''
+        Test that the computed rate from multiple users is correct.
+        In this test, Food name Baozi has 3 user votes and origingal rate is 80.
+        If total for users rate is 120, then the total rate after computed will be 72.
+        '''
         food = Food.objects.all().first()
         food.set_user_rate(120)
 
         rate = food.compute_total_rate()
         self.assertEqual(rate,72)
-        
+
 
 class FixturesTest(TestCase):
     fixtures = ['seed.json']
 
     def test_pk_category_representation(self):
-        ''' seed can be sending data of Category  '''
+        '''
+        Test that seed can send the correct data of Category object.
+        '''
         category = Category.objects.get(pk=5)
         self.assertTrue(Category.objects.filter(type_name='Healthy food'))
 
+
     def test_pk_ethnic_representation(self):
-        ''' seed can be sending data of EthnicFood '''
+        '''
+        Test that seed can send the correct data of EthnicFood object.
+        '''
         ethnic = EthnicFood.objects.get(pk=101)
         self.assertTrue(EthnicFood.objects.filter(ethnic_food_name='Chinese food'))
 
+
     def test_pk_food_all_componet_representation(self):
-        ''' seed can be sending data of Food '''
+        '''
+        Test that seed can send the correct data of Food object.
+        '''
         food = Food.objects.get(pk=1001)
         self.assertEqual(list(food.get_category().all()),[Category.objects.get(pk=5),Category.objects.get(pk=7),Category.objects.get(pk=8)])
         self.assertTrue(Food.objects.filter(food_name='Bak Kut Teh'))
