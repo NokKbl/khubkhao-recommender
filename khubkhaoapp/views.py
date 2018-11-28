e929bbd7c5a5c6a263b6d6e93788ba5f579c87f3
New-az <viyotboon_new@outlook.com>
2018-11-28 18:14:18 +0700

    add logging to save activity of user

--- khubkhaoapp/views.py
+++ khubkhaoapp/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 from khubkhaoapp.models import Category,EthnicFood, Food
 from django.db.models import Q
 from enum import Enum
+from django.contrib.auth.models import User
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class HomeView(TemplateView):
     template_name = 'registration/login.html'
@@ -75,12 +80,22 @@ def IndexView(request):
         'category_list': category_list,
         'ethnic_list': ethnic_list,
     }
+    ip = get_client_ip(request)
+    if request.user.is_authenticated and not request.user.is_anonymous:
+        logger.info('ip: %s id: %d user: %s have been using webpage ' % (ip,request.user.id,request.user.get_full_name()))
+    else:
+        logger.info('ip: %s Guest have been using webpage' % ip)
     return render(request, template_name, context)
     
 def IndexResultView(request):
+    ip = get_client_ip(request)
     if request.method == "POST":
         my_ethnic = request.POST.getlist('ethnic_name')
         my_category = request.POST.getlist('category_name')
+        if request.user.is_authenticated and not request.user.is_anonymous:
+            logger.info('ip: %s id: %d user: %s is selected EhtnicFood%s and Category%s ' % (ip,request.user.id,request.user.get_full_name(),str(my_ethnic),str(my_category) ) )
+        else:
+            logger.info('ip: %s Guest is selected %s and %s ' % (ip,str(my_ethnic),str(my_category)))
     selected_ethnic = filter_ethnic(my_ethnic)
     selected_category = filter_category(my_category)
     food_list = filter_food(selected_ethnic,selected_category)
@@ -99,7 +114,7 @@ def IndexVoteView(request):
     if request.method == "POST" and request.user.is_authenticated and not request.user.is_anonymous:
         my_vote = request.POST.get('rate_star')
         pk_and_vote = my_vote.split(',')
-        vote_food(request,pk_and_vote[0],pk_and_vote[1])    
+        vote_food(request,pk_and_vote[0],pk_and_vote[1])
     food_list = Food.objects.all()
     check_vote(request,food_list)
     food_list = sort_food(food_list)
@@ -110,4 +125,14 @@ def IndexVoteView(request):
         'category_list': category_list,
         'ethnic_list': ethnic_list,
     }
+    ip = get_client_ip(request)
+    logger.info('ip: %s id: %d user: %s have been vote food for %s ' % (ip,request.user.id,request.user.get_full_name(),pk_and_vote[1]))
     return render(request,'khubkhaoapp/index.html', context)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
