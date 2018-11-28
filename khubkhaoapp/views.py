fa0add7a31a2d039dae470372615742ca3903543
New-az <viyotboon_new@outlook.com>
2018-11-28 22:14:10 +0700

    logging activity of user

--- khubkhaoapp/views.py
+++ khubkhaoapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.db.models import Q
 from enum import Enum
 from khubkhaoapp.models import Category, EthnicFood, Food
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Rate(Enum):
@@ -53,6 +56,37 @@ def filter_food(selected_ethnic,selected_category):
         category__in=selected_category).distinct()
 
 
+def logging_user(request,selected_ethnic,selected_category):
+    '''
+    Logging activity that user select type of ethnic food, category.
+    '''
+    user_ip = get_client_ip(request)
+    category = str(selected_category)
+    ethnic = str(selected_ethnic)
+
+    if check_authenticated(request):
+        user_id = request.user.id
+        username = request.user.get_full_name()
+
+        if not selected_category.exists() and not selected_ethnic.exists():
+            logger.info('ip: %s id: %d user: %s didn\'t selected' % (user_ip,user_id,username))
+        elif selected_category.exists() and not selected_ethnic.exists():
+            logger.info('ip: %s id: %d user: %s is selected Category%s' % (user_ip,user_id,username,category))
+        elif not selected_category.exists() and selected_ethnic.exists():
+            logger.info('ip: %s id: %d user: %s is selected EthnicFood%s' % (user_ip,user_id,username,ethnic))
+        else:
+            logger.info('ip: %s id: %d user: %s is selected EthnicFood%s and Category%s ' % (user_ip,user_id,username,ethnic,category))
+    else:
+        if not selected_category.exists() and not selected_ethnic.exists():
+            logger.info('ip: %s Guest didn\'t selected' % (user_ip))
+        elif selected_category.exists() and not selected_ethnic.exists():
+            logger.info('ip: %s Guest is selected Category%s' % (user_ip,category))
+        elif not selected_category.exists() and selected_ethnic.exists():
+            logger.info('ip: %s Guest is selected EthnicFood%s' % (user_ip,ethnic))
+        else:
+            logger.info('ip: %s Guest is selected EthnicFood%s and Category%s ' % (user_ip,ethnic,category))
+
+        
 def sort_food(unsorted_food):
     '''
     Sort food by overall rate.
@@ -85,6 +119,12 @@ def vote_food(request,pk_food,vote):
     food.save()
 
 
+def check_authenticated(request):
+    if request.user.is_authenticated and not request.user.is_anonymous:
+        return True
+    return False
+
+
 class HomeView(TemplateView):
     template_name = 'registration/login.html'
 
@@ -93,11 +133,19 @@ def IndexView(request):
     '''
     Initial template when user get into KHUBKHAO-RECOMMENDER.
     '''
+    user_ip = get_client_ip(request)
     template_name = 'khubkhaoapp/index.html'
-    if request.method == "POST" and request.user.is_authenticated and not request.user.is_anonymous:
-        my_vote = request.POST.get('rate_star')
-        pk_and_vote = my_vote.split(',')
-        vote_food(request,pk_and_vote[0],pk_and_vote[1])
+    if check_authenticated(request):
+        user_id = request.user.id
+        username = request.user.get_full_name()
+        logger.info('ip: %s id: %d user: %s have been using webpage ' % (user_ip,user_id,username))
+        if request.method == "POST":
+            my_vote = request.POST.get('rate_star')
+            pk_and_vote = my_vote.split(',')
+            vote_food(request,pk_and_vote[0],pk_and_vote[1])
+            logger.info('ip: %s id: %d user: %s have been vote food for %s ' % (user_ip,user_id,username,pk_and_vote[1]))
+    else:
+        logger.info('ip: %s Guest have been using webpage' % user_ip)
     unsorted_results = Food.objects.all()
     check_vote(request,unsorted_results)
     food_list = sort_food(unsorted_results)
@@ -121,6 +169,7 @@ def IndexResultView(request):
         my_category = request.POST.getlist('category_name')
         selected_ethnic = filter_ethnic(my_ethnic)
         selected_category = filter_category(my_category)
+        logging_user(request,selected_ethnic,selected_category)
         food_list = filter_food(selected_ethnic,selected_category)
     else:
         food_list = Food.objects.all()
@@ -134,3 +183,11 @@ def IndexResultView(request):
         'ethnic_list': ethnic_list,
     }
     return render(request,template_name, context)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
