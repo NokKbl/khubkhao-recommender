2e6c15567d9cbd0a4a2288ae61ce6aa92e2baf11
New-az <viyotboon_new@outlook.com>
2018-11-28 11:40:13 +0700

    Improve view.py to handle invalid requests.

--- khubkhaoapp/urls.py
+++ khubkhaoapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from khubkhaoapp.views import IndexView, IndexResultView, HomeView, IndexVoteView
+from khubkhaoapp.views import IndexView, IndexResultView, HomeView #IndexVoteView
 
 app_name = 'khubkhaoapp'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('khubkhao/', IndexView, name='index'),
     path('result/', IndexResultView, name='result'),
-    path('vote/', IndexVoteView, name='vote'),
+    # path('vote/', IndexVoteView, name='vote'),
 ]
--- khubkhaoapp/views.py
+++ khubkhaoapp/views.py
@@ -28,12 +28,21 @@ def vote_value(raw_number):
         return Rate.FIVE
 
 def filter_ethnic(ethnic):
+    '''
+    Get ethnic from user then filter EhtnicFood.
+    '''
     return EthnicFood.objects.filter(id__in=ethnic)
 
 def filter_category(category):
+    '''
+    Get category from user then filter Category.
+    '''
     return Category.objects.filter(id__in=category)
 
 def filter_food(selected_ethnic,selected_category):
+    '''
+    Get selected_ethnic and selected_category from user then filter Food.
+    '''
     if not selected_category.exists() and not selected_ethnic.exists():
         return Food.objects.all()
     elif not selected_category.exists() or not selected_ethnic.exists():
@@ -43,9 +52,15 @@ def filter_food(selected_ethnic,selected_category):
         category__in=selected_category).distinct()
 
 def sort_food(unsort_food):
+    '''
+    Sort food by overall rate.
+    '''
     return sorted(unsort_food, key = lambda food: food.compute_total_rate(), reverse=True)[:25]
 
 def check_vote(request,food_set):
+    '''
+    If user vote food then set check to false.
+    '''
     for food in food_set:
         array = food.get_user_pk().split(',')
         if(str(request.user.pk) in array):
@@ -56,6 +71,9 @@ def check_vote(request,food_set):
 
 
 def vote_food(request,pk_food,vote):
+    '''
+    If user vote food then count of vote in each food is increase.
+    '''
     vote_scores = vote_value(vote).value
     food = Food.objects.get(pk=pk_food)
     food.add_user_count()
@@ -64,7 +82,14 @@ def vote_food(request,pk_food,vote):
     food.save()
 
 def IndexView(request):
+    '''
+    Initial template when user get into KHUBKHAO-RECOMMENDER.
+    '''
     template_name = 'khubkhaoapp/index.html'
+    if request.method == "POST" and request.user.is_authenticated and not request.user.is_anonymous:
+        my_vote = request.POST.get('rate_star')
+        pk_and_vote = my_vote.split(',')
+        vote_food(request,pk_and_vote[0],pk_and_vote[1])
     unsorted_results = Food.objects.all()
     check_vote(request,unsorted_results)
     food_list = sort_food(unsorted_results)
@@ -78,36 +103,29 @@ def IndexView(request):
     return render(request, template_name, context)
     
 def IndexResultView(request):
+    '''
+    If user filter food then POST request to change result of food.
+    '''
+    template_name = 'khubkhaoapp/index.html'
     if request.method == "POST":
         my_ethnic = request.POST.getlist('ethnic_name')
         my_category = request.POST.getlist('category_name')
-    selected_ethnic = filter_ethnic(my_ethnic)
-    selected_category = filter_category(my_category)
-    food_list = filter_food(selected_ethnic,selected_category)
-    check_vote(request,food_list)
-    food_list = sort_food(food_list)
-    category_list = Category.objects.all()
-    ethnic_list = EthnicFood.objects.all()
-    context = {
-        'food_list': food_list,
-        'category_list': category_list,
-        'ethnic_list': ethnic_list,
-    }
-    return render(request, 'khubkhaoapp/index.html', context)
+        selected_ethnic = filter_ethnic(my_ethnic)
+        selected_category = filter_category(my_category)
+        food_list = filter_food(selected_ethnic,selected_category)
+        check_vote(request,food_list)
+        food_list = sort_food(food_list)
 
-def IndexVoteView(request):
-    if request.method == "POST" and request.user.is_authenticated and not request.user.is_anonymous:
-        my_vote = request.POST.get('rate_star')
-        pk_and_vote = my_vote.split(',')
-        vote_food(request,pk_and_vote[0],pk_and_vote[1])    
-    food_list = Food.objects.all()
-    check_vote(request,food_list)
-    food_list = sort_food(food_list)
+    else:
+        unsorted_results = Food.objects.all()
+        check_vote(request,unsorted_results)
+        food_list = sort_food(unsorted_results)
+
     category_list = Category.objects.all()
-    ethnic_list = EthnicFood.objects.all()
+    ethnic_list = EthnicFood.objects.all()    
     context = {
         'food_list': food_list,
         'category_list': category_list,
         'ethnic_list': ethnic_list,
     }
-    return render(request,'khubkhaoapp/index.html', context)
+    return render(request,template_name, context)
